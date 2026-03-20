b4648f5f4dd963bebed63b2ae746c3ecf11e45da
spark1 <artur.mikha@gmail.com>
2026-03-20 16:06:24 +0500

    struggle a lttle

--- Assignment 1/src/Main.java
+++ Assignment 1/src/Main.java
@@ -1,19 +1,30 @@
 void main() {
+    Scanner scan = new Scanner(System.in);
     int n= 0;
     int b = 0;
-    int arr[] = {1,2,3,4,5,6,7,8,9,99};
     //test1
-    n = 4;
+    //
+    n = scan.nextInt();
     System.out.println("Sum of squares(n="+ n +"): " + Recursion.sumOfNSquares(n));
     //test2
-    n = 5;
+    n = scan.nextInt();
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16, 99};
     System.out.println("Sum of array elements(n=" + n + "): " + Recursion.sumOfNArrayElements(arr, n));
     //test3
-    n = 7;
-    System.out.println("sum of positive ints(n=" + n + "):"+ Recursion.sumOfN(n));
+    n = scan.nextInt();
+    System.out.println("Sum of positive ints(n=" + n + "):"+ Recursion.sumOfN(n));
     //test4
-    b = 4;
-    n = 3;
-    System.out.println("sum of n powers of b(n=" + n + "b=" + b + "):"+ Recursion.sumOfNPowersOfB(b,n));
+    b = scan.nextInt();
+    n = scan.nextInt();
+    System.out.println("Sum of n powers of b(n=" + n + "b=" + b + "):"+ Recursion.sumOfNPowersOfB(b,n));
     //test5
+    Recursion.reverseNElemnts(n, scan);
+    n = scan.nextInt();
+    scan.close();
+    //test6
+    n = scan.nextInt();
+    Recursion.reverseNLines(n, scan);
+    scan.close();
+    //test7
+
 }
--- Assignment 1/src/Recursion.java
+++ Assignment 1/src/Recursion.java
@@ -28,8 +28,24 @@ public class Recursion {
         reverseNElemnts(n - 1, scan);
         System.out.print(x + " ");
     }
+    //task6
+    public static void reverseNLines(int n, Scanner scan){
+        if (n == 0) return;
+        char[] str = scan.nextLine().toCharArray();
+        reverseNLines(n - 1, scan);
+        System.out.print(str);
+    }
+    //task7
+    public static void spiralN(int n){
+
+    }
+    //task8
+    public static void sequenceNK(int n, int k){
+        if() return ;
+
+    }
     //aditional funcs
-    public static int power(int a, int b) {
+    private static int power(int a, int b) {
         if (b == 0) return 1;
         if (b == 1) return a;
         return a*power(a, b-1);
